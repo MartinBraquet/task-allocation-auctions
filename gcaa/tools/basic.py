a4c72c2dbdd3687f96873058df88de6e25a1aac4
MartinBraquet <martin.braquet@gmail.com>
2025-12-06 18:06:25 +0100

    Fix tests

--- gcaa/tools/basic.py
+++ gcaa/tools/basic.py
@@ -1,8 +1,6 @@
 from collections import defaultdict
 
 import numpy as np
-import torch
-
 
 class PrettyDict(dict):
     def __repr__(self):
@@ -15,21 +13,14 @@ def dict_factory():
 
 def check_nan(data):
     """
-    >>> check_nan(torch.Tensor([1, 2, 3]))
-    >>> check_nan(np.array([1, 2, 3]))
     >>> check_nan(None)
-    >>> check_nan(torch.Tensor([1, float('nan'), 3]))
-    Traceback (most recent call last):
-       ...
-    RuntimeError: NaN detected in: tensor([1., nan, 3.])
+    >>> check_nan(np.array([1, 2, 3]))
     >>> check_nan(np.array([1, float('nan'), 3]))
     Traceback (most recent call last):
        ...
     RuntimeError: NaN detected in: [ 1. nan  3.]
     """
     if data is None:
-        return
-    if isinstance(data, torch.Tensor) and not torch.isnan(data).any():
         return
     if isinstance(data, np.ndarray) and not np.isnan(data).any():
         return
--- scripts/test_mypi.sh
+++ scripts/test_mypi.sh
@@ -2,7 +2,6 @@
 
 conda create -n test python=3.12 pip -y
 conda activate test
-pip install torch --index-url https://download.pytorch.org/whl/cpu
 pip install gcaa
 python -c "from gcaa import GCAA; GCAA().run()"
 echo "Installation from Pypi ran a test successfully"
